2ccbcc049a4e33caa625ce90122ed191426180bb
mcpar-land <john@mcpar.land>
2020-12-11 11:08:34 -0600

    improve part 1

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,5 +1,3 @@
-use indicatif::ProgressBar;
-
 pub fn day01(_: Vec<String>) -> () {
 	println!("Day 1!");
 	let input_raw: String = crate::util::read_input("day01.txt");
@@ -8,22 +6,17 @@ pub fn day01(_: Vec<String>) -> () {
 		.map(|n| n.parse::<u32>().unwrap())
 		.collect::<Vec<u32>>();
 
-	let spinner_one = ProgressBar::new_spinner();
 	'one: for i in 0..input.len() {
 		for j in i..input.len() {
 			let a = input[i];
 			let b = input[j];
 			let res = a + b;
-			spinner_one.set_message(&format!("{} + {} = {}", a, b, res));
-			spinner_one.tick();
 			if res == 2020 {
-				spinner_one.finish();
 				println!("Result found! {} * {} = {}", a, b, a * b);
 				break 'one;
 			}
 		}
 	}
-	let spinner_two = ProgressBar::new_spinner();
 	'two: for i in 0..input.len() {
 		for j in i..input.len() {
 			for k in j..input.len() {
@@ -31,9 +24,8 @@ pub fn day01(_: Vec<String>) -> () {
 				let b = input[j];
 				let c = input[k];
 				let res = a + b + c;
-				spinner_two.set_message(&format!("{} + {} + {} = {}", a, b, c, res));
+
 				if res == 2020 {
-					spinner_two.finish();
 					println!("Result found! {} * {} * {} = {}", a, b, c, a * b * c);
 					break 'two;
 				}
